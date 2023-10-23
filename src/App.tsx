646522905060b8b5abe1c09b5e8dc2320a5ef9af
JinYee Jeong <luvdo0112@daum.net>
2023-10-23 11:10:57 +0900

    Add Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
 import "./App.css";
-import MainPage from "./MainPage/MainPage";
+import MainPage from "./MainPage/mainPage";
 import ArticleList from "./ArticleList/ArticleList";
 import { Route, Routes } from "react-router-dom";
+import React from "react";
+import MyLike from "./MyLIke/MyLike";
+import Email from "./Email/Email";
+import LoginPage from "./LoginPage/LoginPage";
+import MyReply from "./MyReply/MyReply";
+import Nickname from "./Nickname/Nickname";
+import Pw from "./Pw/Pw";
+import SettingsPage from "./SettingsPage/SettingsPage";
+import SignupPage from "./SignupPage/SignupPage";
+import UnivShift from "./UnivShift/UnivShift";
+import WritePage from "./WritePage/WritePage";
+import MyPost from "./MyPost/MyPost";
+import Leave from "./Leave/Leave";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <Routes>
       <Route element={<MainPage />} path="/" />
-      <Route element={<ArticleList />} path="/article" />
+      <Route element={<ArticleList />} path="/articlelist" />
+      <Route element={<MyLike />} path="/mylike" />
+      <Route element={<MyPost />} path="/mypost" />
+      <Route element={<Email />} path="/email" />
+      <Route element={<LoginPage />} path="/loginpage" />
+      <Route element={<MyReply />} path="/myreply" />
+      <Route element={<Nickname />} path="/nickname" />
+      <Route element={<Pw />} path="/pw" />
+      <Route element={<SettingsPage />} path="/settingspage" />
+      <Route element={<SignupPage />} path="/signuppage" />
+      <Route element={<UnivShift />} path="/univshift" />
+      <Route element={<WritePage />} path="/writepage" />
+      <Route element={<Leave />} path="/leave" />
     </Routes>
-    // <ArticleList />
   );
 }
 
