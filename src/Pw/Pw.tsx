2136ecb30382d4065d2e44de26fc8baeedf703ce
Sua <sgua0720@gmail.com>
2023-10-29 22:40:54 +0900

    패스워드 다크모드

--- src/Pw/Pw.tsx
+++ src/Pw/Pw.tsx
@@ -3,34 +3,33 @@ import "./Pw.scss";
 
 const Pw = () => {
   return (
-    <div className="position-relative">
-      <h2>비밀번호 변경</h2>
+    <div className="pw">
+      <h2 className="pw__pw-title">비밀번호 변경</h2>
 
-      <div className="input-container">
-        <div className="form-group">
-          <p>현재 PW : </p>
+      <div className="pw__pw-input">
+        <div className="pw__pw-input__pw-group">
+          <span>현재 PW : </span>
           <input
-            className="school-input"
-            type="text"
+            className="pw__pw-input__pw-group__pw-pre"
+            type="password"
             placeholder="현재 비밀번호를 입력하세요"
           />
           <button>확인</button>
         </div>
 
-        <div className="form-group">
-          <p>새 PW : </p>
+        <div className="pw__pw-input__pw-group">
+          <span>새 PW : </span>
           <input
-            className="school-input"
-            type="text"
+            className="pw__pw-input__pw-group__pw-new"
+            type="password"
             placeholder="변경할 비밀번호를 입력하세요"
           />
           <button>확인</button>
         </div>
       </div>
-      <button className="submit-btn">완료</button>
+      <button className="pw__pw-btn">완료</button>
     </div>
   );
 };
 
 export default Pw;
-//ㅇㅇ
