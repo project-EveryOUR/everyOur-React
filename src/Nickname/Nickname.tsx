2d1f131351ab1c830488ac9f6e20c88a623b68a5
Sua <sgua0720@gmail.com>
2023-10-29 22:43:32 +0900

    닉네임 다크모드

--- src/Nickname/Nickname.tsx
+++ src/Nickname/Nickname.tsx
@@ -5,25 +5,25 @@ const Nickname = () => {
   const handleButtonClick = () => {
     alert("수정되었습니다");
   };
-  return (
-    <div className="position-relative">
-      <h2>닉네임 변경</h2>
 
-      <div className="input-container">
-        <div className="form-group">
-          <p>새 닉네임 : </p>
+  return (
+    <div className="nickname">
+      <h2 className="nickname__nick-title">닉네임 변경</h2>
+
+      <div className="nickname__nick-input">
+        <div className="nickname__nick-input__nick-group">
+          <span>새 닉네임 : </span>
           <input
-            className="school-input"
+            className="nickname__nick-input__nick-group__nick-new"
             type="text"
             placeholder="새 닉네임을 입력하세요"
           />
           <button onClick={handleButtonClick}>수정</button>
         </div>
       </div>
-      <button className="submit-btn">완료</button>
+      <button className="nickname__nick-btn">완료</button>
     </div>
   );
 };
 
 export default Nickname;
-//엥
