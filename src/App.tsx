1142b675a9aed44d5d6d1cca4fa4ab23e2221b9b
yunsejin <ysj1375@naver.com>
2023-11-10 21:03:53 +0900

    App.tsx2.0

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,9 @@ import HotArticleList from "./ArticleList/HotArticleList/HotArticleList";
 import InfoArticleList from "./ArticleList/InfoArticleList/InfoArticleList";
 import SecretArticleList from "./ArticleList/SecretArticleList/SecretArticleList";
 import PostIn from "./PostIn/PostIn";
-
+import Langset from "./LangsetPage/LangsetPage";
+import UsePage from "./UsePage/UsePage";
+import InfoPage from "./ArticleList/InfoPage/InfoPage";
 function App() {
   return (
     <Routes>
@@ -41,8 +43,11 @@ function App() {
       <Route element={<FreeArticleList />} path="/freearticlelist" />
       <Route element={<HotArticleList />} path="/hotarticlelist" />
       <Route element={<InfoArticleList />} path="/infoarticlelist" />
-      <Route element={<SecretArticleList />} path="/Secretarticlelist" />
-      <Route element={<PostIn />} path="/PostIn" />
+      <Route element={<SecretArticleList />} path="/secretarticlelist" />
+      <Route element={<PostIn />} path="/postIn" />
+      <Route element={<Langset />} path="/langset" />
+      <Route element={<UsePage />} path="/usepage" />
+      <Route element={<InfoPage />} path="/infopage" />
     </Routes>
   );
 }
