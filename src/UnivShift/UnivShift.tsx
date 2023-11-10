38d58b294d60953502100fc1acd9f5361594f8c8
yunsejin <ysj1375@naver.com>
2023-11-10 21:01:56 +0900

    UnivShift2.0

--- src/UnivShift/UnivShift.tsx
+++ src/UnivShift/UnivShift.tsx
@@ -6,44 +6,56 @@ const UnivShift = () => {
     alert("수정되었습니다");
   };
   return (
-    <div className="position-relative">
-      <h2>학교 설정</h2>
-      <p className="menual">
+    <div className="univshift">
+      <h2 className="univshift__univ-title">학교 설정</h2>
+      <p className="univshift__univ-menual">
         학교 변경은 변경된 학교의 이메일 인증 후 자동으로 변경됩니다. 문의
         사항은 031-123-234으로 해주세요.
       </p>
-      <div className="input-container">
-        <div className="form-group">
+      <div className="univshift__univ-container">
+        <div className="univshift__univ-container__univ-form">
           <p>학교 변경 : </p>
           <input
-            className="school-input"
+            className="univshift__univ-container__univ-form__univ-input"
             type="text"
             placeholder="이메일을 입력하세요"
           />
-          <button className="okbutton">인증</button>
+          <button className="univshift__univ-container__univ-form__univ-okbtn">
+            인증
+          </button>
         </div>
 
-        <div className="form-group">
+        <div className="univshift__univ-container__univ-form">
           <p>학과 변경 : </p>
           <input
-            className="major-input-box"
+            className="univshift__univ-container__univ-form__univ-major"
             type="text"
             placeholder="학과를 입력하세요"
           />
-          <button onClick={handleButtonClick}>수정</button>
+          <button
+            className="univshift__univ-container__univ-form__univ-modibtn"
+            onClick={handleButtonClick}
+          >
+            수정
+          </button>
         </div>
 
-        <div className="form-group">
+        <div className="univshift__univ-container__univ-form">
           <p>학번 변경 : </p>
           <input
-            className="id-input-box"
+            className="univshift__univ-container__univ-form__univ-id"
             type="text"
             placeholder="학번을 입력하세요"
           />
-          <button onClick={handleButtonClick}>수정</button>
+          <button
+            className="univshift__univ-container__univ-form__univ-modibtn"
+            onClick={handleButtonClick}
+          >
+            수정
+          </button>
         </div>
       </div>
-      <button className="submit-btn">완료</button>
+      <button className="univshift__univ-btn">완료</button>
     </div>
   );
 };
