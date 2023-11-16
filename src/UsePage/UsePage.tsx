42a4d1d9f1e3b4c02a3686a1810f0e00c026f22f
yunsejin <ysj1375@naver.com>
2023-11-16 15:34:23 +0900

    UsePage2.0

--- src/UsePage/UsePage.tsx
+++ src/UsePage/UsePage.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./UsePage.scss";
+import Backbtn from "../assets/Backbtn.svg";
+import { Link, useNavigate } from "react-router-dom";
+import everyOURLogo from "../assets/logo.svg";
 
 const UsePage = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="usepage">
+      <Link to={"/"}>
+        <img
+          src={everyOURLogo}
+          alt="everyOURLogo"
+          className="usepage__Logo"
+        />
+      </Link>
       <h2 className="usepage__usepage-title">사용 문의</h2>
       <br></br>
       <div className="usepage__usebox">
@@ -12,6 +27,8 @@ const UsePage = () => {
         </p>
         <p className="usepage__usebox__usepage_insta">@chamwhea</p>
       </div>
+      <img src={Backbtn} alt="Backbtn" className="usepage__Backbtn" onClick={goBack} />
+      
     </div>
   );
 };
