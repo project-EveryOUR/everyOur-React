34720e07d1b3226c45198521e1df95f21f397531
yunsejin <ysj1375@naver.com>
2023-11-16 15:31:32 +0900

    Langset2.0

--- src/LangsetPage/LangsetPage.tsx
+++ src/LangsetPage/LangsetPage.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./LangsetPage.scss";
+import Backbtn from "../assets/Backbtn.svg";
+import everyOURLogo from "../assets/logo.svg";
 
 const Langset: React.FC = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="Langset">
+      <Link to={"/"}>
+        <img
+          src={everyOURLogo}
+          alt="everyOURLogo"
+          className="Langset__Logo"
+        />
+      </Link>
       <div className="Langset__InternalFrame"></div>
       <div className="Langset__Langsetbanner">언어설정</div>
       <div className="Langset__Korean">한국어</div>
@@ -23,7 +37,7 @@ const Langset: React.FC = () => {
       <Link to={"/"}>
         <div className="Langset__E-Langsetbtn">선택</div>
       </Link>
-
+      <img src={Backbtn} alt="Backbtn" className="Langset__Backbtn" onClick={goBack} />
     </div>
   );
 };
