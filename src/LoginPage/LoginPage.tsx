cc502778f25fe96cac47577ed916a2f2634bf188
yunsejin <ysj1375@naver.com>
2023-11-23 12:49:54 +0900

    login

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signInWithGoogle } from "../firebase";
+import everyOURLogo from "../assets/logo.svg";
 import "./LoginPage.scss";
+import Backbtn from "../assets/Backbtn.svg";
 
 const LoginPage: React.FC = () => {
   const [emailText, setEmailText] = useState<string>("");
-
+  const [idText, setIdText] = useState<string>("");
+  const [region, setRegion] = useState<string>("경기 남부");
   const handleIdInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length <= 20) {
       setEmailText(e.target.value);
     }
   };
+    const handleRegionClick = () => {
+    const newRegion = region === "경기 남부" ? "경기 북부" : "경기 남부";
 
+    window.history.pushState(null, "", `/loginpage/${newRegion.toLowerCase()}`);
+    
+    setRegion(newRegion);
+  };
   const navigate = useNavigate();
 
+  const goBack = () => {
+    navigate(-1);
+  };
   const handleGoogleLogin = () => {
     signInWithGoogle().then(() => {
       // 로그인 성공 시 메인 페이지로 이동
@@ -23,6 +35,14 @@ const LoginPage: React.FC = () => {
 
   return (
     <div className="iFrame">
+      <Link to={"/"}>
+          <img
+            src={everyOURLogo}
+            alt="everyOUR 메인 로고"
+            className="iFrame__Logo"
+          />
+      </Link>
+      <img src={Backbtn} alt="Backbtn" className="iFrame__Backbtn" onClick={goBack} />
       <div className="iFrame__InternalFrame"></div>
       <div className="iFrame__Loginbanner">Login</div>
       <div className="iFrame__EmailInput">
@@ -41,8 +61,9 @@ const LoginPage: React.FC = () => {
       <Link to={"/Signuppage"}>
         <div className="iFrame__Signupbtn"> SignUp</div>
       </Link>
-      <div className="iFrame__GyeonggiSouth">경기 남부</div>
-      <div className="iFrame__GyeonggiSouthframe"></div>
+      <div className="iFrame__GyeonggiSouth" onClick={handleRegionClick}>
+        {region}
+      </div>
     </div>
   );
 };
