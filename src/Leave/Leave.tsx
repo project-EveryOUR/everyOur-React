815949a0b5121e6ddfc46be48dc16a46ff0487dd
yunsejin <ysj1375@naver.com>
2023-11-23 12:49:40 +0900

    leave

--- src/Leave/Leave.tsx
+++ src/Leave/Leave.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./Leave.scss";
 import Backbtn from "../assets/Backbtn.svg";
 import { Link, useNavigate } from "react-router-dom";
+import everyOURLogo from "../assets/logo.svg";
+
+
 const Leave = () => {
   const [isChecked, setIsChecked] = useState(false);
 
@@ -15,6 +18,13 @@ const Leave = () => {
   };
   return (
     <div className="leave">
+      <Link to={"/"}>
+        <img
+          src={everyOURLogo}
+          alt="everyOURLogo"
+          className="Langset__Logo"
+        />
+      </Link>
        <img src={Backbtn} alt="Backbtn" className="leave__Backbtn" onClick={goBack} />
       <h2 className="leave__leave-title">탈퇴</h2>
       <p className="leave__leave-menual">
@@ -26,7 +36,7 @@ const Leave = () => {
         </p>
         <p className="leave__readbox__content-1">
           개인형 서비스 이용기록은 모두 삭제되며, 삭제된 데이터는 복구되지
-          않습니다. 삭제되는 내용을 확인하시고 필요한 데이터는 미리 백업을
+          않습니다. <br />삭제되는 내용을 확인하시고 필요한 데이터는 미리 백업을
           해주세요.
         </p>
 
@@ -34,14 +44,14 @@ const Leave = () => {
           탈퇴 후에도 게시판형 서비스에 등록한 게시물은 그대로 남아 있습니다.
         </p>
         <p className="leave__readbox__content-2">
-          게시글 및 댓글은 탈퇴 시 자동 삭제되지 않고 그대로 남아 있습니다.
-          삭제를 원하는 게시글이 있다면 반드시 탈퇴 전 삭제하시기 바랍니다. 탈퇴
-          후에는 회원정보가 삭제되어 본인 여부를 확인할 수 있는 방법이 없어,
+          게시글 및 댓글은 탈퇴 시 자동 삭제되지 않고 그대로 남아 있습니다.<br />
+          삭제를 원하는 게시글이 있다면 반드시 탈퇴 전 삭제하시기 바랍니다.<br /> 탈퇴
+          후에는 회원정보가 삭제되어 본인 여부를 확인할 수 있는 방법이 없어,<br />
           게시글을 임의로 삭제해드릴 수 없습니다.
         </p>
       </div>
       <p className="leave__content-3">
-        탈퇴 후에는다시 가입할 수 없으며 아이디와 데이터는 복구할 수 없습니다.
+        탈퇴 후에는다시 가입할 수 없으며 아이디와 데이터는 복구할 수 없습니다.<br />
         게시판형 서비스에 남아 있는 게시글은 탈퇴 후 삭제할 수 없습니다.
       </p>
 
